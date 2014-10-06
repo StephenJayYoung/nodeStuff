589c78e821ce55c37b2f8218559723c82cc104ba
Stephen Young <StephenJayYoung@gmail.com>
2014-10-06 11:24:07 -0700

    finished questions 1, 2, 7

---
+++ question7.js
@@ -0,0 +1,13 @@
+nodeschool - http-client problem: var http = require("http");
+
+http.get(process.argv[2], function(res) {
+ // console.log("Got response: " + res.statusCode);
+ res.setEncoding('utf8');
+ res.on("data", function (data) {
+   console.log(data);
+ });
+
+}).on('error', function(e) {
+ console.log("Got error: " + e.message);
+});
+
